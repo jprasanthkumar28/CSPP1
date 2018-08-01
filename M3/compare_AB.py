71339eee4ef006bd4724b4f666ed84415667443c
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-01 12:54:08 +0530

    prashu_Var

---
+++ M3/compare_AB.py
@@ -0,0 +1,11 @@
+varA = 80
+varB = "prashu"
+if type(varA)==str or type(varB)==str:
+	print("string involved")
+elif varA>varB:
+	print("bigger")
+elif varA==varB:
+	print("equal")
+else:
+	print("smaller")
+	
