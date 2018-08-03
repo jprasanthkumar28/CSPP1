f31d66d06be8e538297129efebd5eba4e7a65032
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-03 09:40:01 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,6 +1,6 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the longest substring of s in which the 
+Write a program that prints the longest substring of s in which the
 letters occur in alphabetical order.
 For example, if s = 'azcbobobegghakl', then your program should print
 Longest substring in alphabetical order is: beggh
@@ -16,19 +16,19 @@ had a break and cleared your head.'''
 def main():
     '''Sequence'''
     string1 = input()
-    bigstr=""
+    bigstr = ""
     len1 = len(string1)
     for j in range(len1):
         substr = ""
         dup = string1[j]
-        for i in range(j,len(string1)):
+        for i in range(j, len(string1)):
             if dup <= string1[i]:
                 dup = string1[i]
                 substr += dup
             else:
                 break
         if len(bigstr) < len(substr):
-                bigstr = substr
+            bigstr = substr
     print(bigstr)
     # the input string is in s
     # remove pass and start your code here
