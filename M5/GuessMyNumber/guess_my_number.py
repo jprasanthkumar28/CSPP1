aa97c388f8e00fd82468b3ae7c017572d15566ba
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-04 17:15:45 +0530

    Guess

--- M5/GuessMyNumber/guess_my_number.py
+++ M5/GuessMyNumber/guess_my_number.py
@@ -1,8 +1,24 @@
+'''Guessing_Number'''
 #Guess My Number Exercise
 
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    '''Guessing_Number'''
+    #s = raw_input()
+    #your code here
+    half = 50
+    end = 100
+    start = 0
+    data = 'l'
+    while data != 'c':
+        print(half)
+        data = input("Enter 'h' if guess is too high, 'l' if its too low.\
+            'c' to indicate I guessed correctly")
+        if data == 'h':
+            end = half
+            half = (end+start)//2
+        elif data == 'l':
+            start = half
+            half = (end+start)//2
+    print("Your lucky number is :", half)
+if __name__ == "__main__":
+    main()
