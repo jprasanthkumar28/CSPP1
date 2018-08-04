cd2fa77887d718d460061320184352bf84b5a779
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-04 10:07:31 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- sample1.py
+++ sample1.py
@@ -1,15 +1,43 @@
-x = 23
-epsilon = 0.01
-step = 0.1
-guess = 0.0
-
-while abs(guess**2-x) >= epsilon:
-    if guess <= x:
-        guess += step
-    else:
-        break
-
-if abs(guess**2 - x) >= epsilon:
-    print('failed')
+'''*******Palindrome**********
+value = input()
+str = ""
+for i in value:
+	str = i + str
+if value == str:
+	print("Palindrome")
 else:
-    print('succeeded: ' + str(guess))
+	print("Not Palindrome")'''
+'''*********Reverse of String**********
+value = input()
+str = ""
+for i in value:
+	str = i + str
+print(str)'''
+'''**************To print in vertical way*************
+value = input()
+for i in value:
+	print(i)'''
+'''value = input()
+for i in value:
+	str = i + str
+print(str)'''
+'''*****Vowels and Consonents***********
+value = input()
+vow =""
+cons=""
+l = len(value)
+for i in range(0,l,1):
+	if value[i] in ['a', 'e', 'i', 'o', 'u']:
+		vow += value[i]
+	else:
+		cons += value[i]
+print(vow)
+print(cons)'''
+
+value =input()
+count = len(value)
+for i in range(0,count,1):
+	if value[i].islower()==1:
+		print(value[i].upper(),end="")
+	else:
+		print(value[i].lower(),end="")
