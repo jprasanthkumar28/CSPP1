54bfa302b238bdce58d8f61976fb611c065629f0
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-08 10:33:16 +0530

    Odd_Tuples

---
+++ M9/Odd Tuples Exercise/odd_tuples.py
@@ -0,0 +1,25 @@
+'''Odd_Tuples'''
+#Exercise : Odd Tuples
+#Write a python function oddTuples(aTup)
+#that takes a some numbers in the tuple as input
+#and returns a tuple in which contains odd index values in the input tuple
+
+def oddtuples(atup):
+    '''Odd_Tuples'''
+    #aTup = ('I', 'am', 'a', 'test', 'tuple')
+    new = ()
+    for i in range(0, len(atup), 2):
+        new += (atup[i], )
+    return new
+
+def main():
+    '''Odd_Tuples'''
+    data = input()
+    data = data.split()
+    atup = ()
+    for j in enumerate(len(data)):
+        atup += ((data[j]), )
+    print(oddtuples(atup))
+
+if __name__ == "__main__":
+    main()
