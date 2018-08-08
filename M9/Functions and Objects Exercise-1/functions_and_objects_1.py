a17c56db83b0051b7492d144a7e7de13907dad0f
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-08 19:15:30 +0530

    Func1

--- M9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ M9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -1,15 +1,15 @@
 '''Abs'''
 #Exercise : Function and Objects Exercise-1
-#Implement a function that converts the given 
+#Implement a function that converts the given
 #testList = [1, -4, 8, -9] into [1, 4, 8, 9]
 
-def apply_to_each(testList, f):
-	'''Abs'''
-	p = map(abs,testList)
-	return list(p)
+def apply_to_each(testlist, _):
+    '''Abs'''
+    result = map(abs, testlist)
+    return list(result)
 
 def main():
-	'''Abs'''
+    '''Abs'''
     data = input()
     data = data.split()
     list1 = []
