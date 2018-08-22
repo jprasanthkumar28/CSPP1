810d99d39172ac65e29fa4eeba2b521dda8efce2
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-22 10:08:19 +0530

    Accio_Activity5

--- M15/accio.py
+++ M15/accio.py
@@ -17,6 +17,9 @@ class Accio(Spell):
     def __init__(self):
         Spell.__init__(self, 'Accio', 'Summoning Charm')
     
+    def __str__(self):
+        return self.name + ' ' + self.incantation + '\n' + self.getDescription()
+
     def getDescription(self):
         return 'This charm summons an object to the caster, potentially over a significant distance.'
 
@@ -31,6 +34,6 @@ def studySpell(spell):
     print(spell)
 
 spell = Accio()
-spell.execute()
+# spell.execute()
 studySpell(spell)
-studySpell(Confundo())
+# studySpell(Confundo())
