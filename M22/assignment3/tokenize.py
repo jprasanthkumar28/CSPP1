f9c5008d39fddffcb9deaf6324790eb8c5d38907
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-08-25 12:52:43 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- M22/assignment3/tokenize.py
+++ M22/assignment3/tokenize.py
@@ -4,23 +4,25 @@ each word
 '''
 import re
 def tokenize(string):
+    '''Function to tokenize'''
     # pass
     newdict = {}
     string = re.sub('[^A-Z,a-z,0-9 ]', '', string)
     string = string.split()
     for word in string:
-   		if word in newdict:
-   			newdict[word] += 1
-   		else:
-   			# newdict[word][0].append(docs.index(line))
-   			newdict[word] = 1
-    return newdict 
-            
+        if word in newdict:
+            newdict[word] += 1
+        else:
+            # newdict[word][0].append(docs.index(line))
+            newdict[word] = 1
+    return newdict
+
 def main():
+    '''Main Function'''
     doc = ''
     lines = int(input())
-    for index in range(lines):
-    	doc = ''.join(input())
+    for _ in range(lines):
+        doc = ''.join(input())
     print(tokenize(doc))
 
 if __name__ == '__main__':
