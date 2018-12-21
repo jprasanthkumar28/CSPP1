6e773ac10f50367c4ebb256748922434d9fd01b6
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2018-12-21 12:14:40 +0530

    "cspp1_m10_a1_1: 9/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

---
+++ R-Exam/CSPP-1_Question/Sudoku.py
@@ -0,0 +1,26 @@
+data = input()
+check = [False] * 9
+if '.' not in data:
+    print("Given sudoku is solved")
+elif len(data) != 81:
+    print("Invalid input")
+else:
+    count = 0
+    string = ''
+    for string in data:
+        if count == 9:
+            for index in range(9):
+                if check[index] == False:
+                    print(index + 1)
+            check = [False] * 9
+            count = 0
+        count = count + 1
+        if(string == '.'):
+            continue
+        check[int(string) - 1] = True
+    if count == 9:
+	    for j in range(9):
+	        if check[j] == False:
+	            print(j + 1)
+	    check = [False] * 9
+	    count = 0
