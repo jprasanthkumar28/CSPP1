88c463c37ece7a29a89c26f51163e172edd05388
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-03 12:23:21 +0530

    "cspp2_m10_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

--- R-Exam/CSPP2/CSPP2 Remedial Exam/Reservation.py
+++ R-Exam/CSPP2/CSPP2 Remedial Exam/Reservation.py
@@ -1,31 +1,18 @@
-def reserve(data, room_Num):
-    count = 5
-    if room_Num == {}:
-        room_Num[1] = data[1]
-    else:
-        for i in range(1,count+1):
-            if i not in room_Num.keys():
-                room_Num[i] = data[1]
-                # print(data[1], i)
-                break
-            # print(i)
-        # room_Num = 
-        # sorted
-    # return room_Num
-    # return room_Num.keys()
-    # for key in sorted(room_Num):
-    #         print(room_Num[key], key)
-
 def main():
-    # global room_Num
     room_data = {}
     number = int(input())
-    # print(number)
     for i in range(number):
         choice = input().split()
         if choice[0] == "reserve":
-            # print("reserve")
-            reserve(choice,room_data)
+            count = 5
+            if room_data == {}:
+                room_data[1] = data[1]
+            else:
+                for i in range(1,count+1):
+                    if i not in room_data.keys():
+                        room_data[i] = data[1]
+                        # print(data[1], i)
+                        break
         elif choice[0] == "reserveN":
             room_data[int(choice[2])] = choice[1]
     for key, value in room_data.items():
