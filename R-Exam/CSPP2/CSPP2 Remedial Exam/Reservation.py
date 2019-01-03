a0c6a84944d0f6cf80cbeaab33092b40de4b8aef
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-03 13:59:10 +0530

    "cspp2_m10_a1_1: 1/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 7/10 "

--- R-Exam/CSPP2/CSPP2 Remedial Exam/Reservation.py
+++ R-Exam/CSPP2/CSPP2 Remedial Exam/Reservation.py
@@ -1,25 +1,40 @@
 def main():
     room_data = {}
+    count = 6
     number = int(input())
     for i in range(number):
         choice = input().split()
         if choice[0] == "reserve":
-            count = 5
             if room_data == {}:
-                room_data[1] = data[1]
+                room_data[1] = choice[1]
+                print(choice[1], 1)
             else:
-                for i in range(1,count+1):
+                for i in range(1,count):
                     if i not in room_data.keys():
-                        room_data[i] = data[1]
-                        # print(data[1], i)
+                        if i >= count:
+                            print("All room are reserved")
+                            break
+                        room_data[i] = choice[1]
+                        print(choice[1], i)
                         break
-        elif choice[0] == "reserveN":
-            room_data[int(choice[2])] = choice[1]
-    for key, value in room_data.items():
-        print(value + " " + str(key))
-    # print(room_data)
-    for key, value in sorted(room_data.items()):
-        print(value + " " + str(key))
+        if choice[0] == "reserveN":
+            if len(room_data) >= count:
+                print("All room are reserved")
+            elif int(choice[2]) in room_data.keys():
+                print("Room is already reserved")
+            else:
+                room_data[int(choice[2])] = choice[1]
+                print(choice[1], choice[2])
+
+
+        if choice[0] == "print":
+            for key, value in sorted(room_data.items()):
+                print(value + " " + str(key))
+    # for key, value in room_data.items():
+    #     print(value + " " + str(key))
+    # # print(room_data)
+    # for key, value in sorted(room_data.items()):
+    #     print(value + " " + str(key))
 
 if __name__ == '__main__':
     main()
