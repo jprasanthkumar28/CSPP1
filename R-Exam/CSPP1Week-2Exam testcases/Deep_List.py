432d704159adcfcdc49db801cebac800c48a115b
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-05 10:55:31 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ R-Exam/CSPP1Week-2Exam testcases/Deep_List.py
@@ -0,0 +1,38 @@
+def total(lst):
+	sum1 = 0
+	for value in lst:
+		try:
+			sum1 += value
+		except TypeError:
+			sum1 += total(value)
+	return sum1
+
+def count(lst):
+	# result = lst.split("]")
+	c = 0
+	string = ""
+	string = ''.join(str(e) for e in lst)
+	# 	string += lst[i]
+	# print(string)
+	for i in range(len(string)):
+		if string[i] == '[':
+			c += 1
+	return c
+
+def main():
+	# lst = []
+	# sum1 = 0
+	data = eval(input())
+	print(total(data))
+	print(count(data))
+	# for value in data:
+	# 	if ']' != value or '[' != value:
+	# 		sum1 += value
+	# 	# for x in value:
+	# 		# print(value)
+
+
+
+
+if __name__ == '__main__':
+	main()
