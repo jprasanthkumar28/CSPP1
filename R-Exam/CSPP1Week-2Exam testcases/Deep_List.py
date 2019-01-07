1416c4cfd0e59a94dca7241225261eeda69fb5ab
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-07 13:51:41 +0530

    "cspp1_m10_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -1/10 "

--- R-Exam/CSPP1Week-2Exam testcases/Deep_List.py
+++ R-Exam/CSPP1Week-2Exam testcases/Deep_List.py
@@ -1,12 +1,22 @@
 def total(lst):
     sum1 = 0
     for value in lst:
-        try:
-            if type(value) != str:
-                sum1 += value
-        except TypeError:
-            sum1 += total(value)
+    #     try:
+    #         if type(value) != str:
+    #             sum1 += value
+    #     except TypeError:
+    #         sum1 += total(value)
+    # return sum1
+            if type(value) != list:
+                # print(value,"LIST")
+                if (type(value) != str):
+                    # print(value,"INT")
+                    sum1 += value
+            else:
+                sum1 += total(value)
     return sum1
+
+
 
 def count(lst):
     # result = lst.split("]")
@@ -20,31 +30,30 @@ def count(lst):
             c += 1
     return c
 
-def getDeep(lst):
-    count = 1
-    for value in lst:
-        if type(value)==type([]):
-            count += 1
-    print(count)
-    # c = []
-    # count = 0
-    # string = ""
-    # string = ''.join(str(e) for e in lst)
-    # # data = list(string)
-    # # print(data)
-    # # for i in data:
-    # for i in range(len(string)):
-    #     # pass
-    #     if string[i] == '[':
+def getDeep(lsts):
+    # count = 1
+    # for value in lst:
+    #     if type(value) == list:
     #         count += 1
-    #         if string[i] == ']':
-    # #         count += 1
-    # #         if i == ']':
-    #             break
-    # # c.append(count)
-    # print(count)
 
-    # print(data)
+    # print(count)
+    # global sublistcount
+    # sublistcount = 0
+    # global depth
+    # depth = 0
+    # def sum_all(lsts):
+        sublistcount = 0
+        s = 0
+        ss = ''
+        for item in lsts:
+            if type(item) is list:
+                sublistcount = sublistcount + 1
+                s += getDeep(item) 
+            elif type(item) is str:
+                ss += item
+            else:
+                s += item
+        return s
 
 def main():
     # lst = []
@@ -52,7 +61,7 @@ def main():
     data = eval(input())
     print(total(data))
     print(count(data))
-    getDeep(data)
+    print(getDeep(data))
     # for value in data:
     #   if ']' != value or '[' != value:
     #       sum1 += value
