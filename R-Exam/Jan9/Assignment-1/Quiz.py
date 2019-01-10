5fed2932f9bf3e31583e9663c0d2b65f0c4c07ae
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-10 17:35:03 +0530

    "m19_a1: 2/13 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- R-Exam/Jan9/Assignment-1/Quiz.py
+++ R-Exam/Jan9/Assignment-1/Quiz.py
@@ -1,67 +1,76 @@
 def load_ques(data):
 	global ques
+	# global flag
 	print("|----------------|")
 	print("| Load Questions |")
 	print("|----------------|")
 	q_count = int(data[1])
-	flag = True
 	# print(q_count)
 	for i in range(q_count):
 		ques.append(input().split(":"))
 	# print(ques)
-		if '' in ques:
+	for item in ques:
+		if '' in item:
 			print("Error! Malformed question")
-			flag = False
+			flag = 1
 		elif q_count == 0:
 			print("Quiz does not have questions")
-		elif flag != False: 
+			flag = 1
+		elif flag != 1: 
 			print(q_count,"are added to the quiz")
 
 def start_quiz(data):
+	# global flag
 	print("|------------|")
 	print("| Start Quiz |")
 	print("|------------|")
-	option = int(data[1])
-	for q in ques:
-		ans = input().split(" ")
-		# print(ans)
-		q.append(ans[1])
-		# print(q)
-		print(q[0],"(",q[3],")")
-		option = q[1].split(",")
-		print(option[0],"\t",option[1],"\t",option[2],"\t",option[3])
-		print()
+	if flag == 1:
+		option = int(data[1])
+		for q in ques:
+			ans = input().split(" ")
+			# print(ans)
+			q.append(ans[1])
+			# print(q)
+			print(q[0],"(",q[3],")")
+			option = q[1].split(",")
+			print(option[0],"\t",option[1],"\t",option[2],"\t",option[3])
+			print()
 
 def display(data):
+	# global flag
 	print("|--------------|")
 	print("| Score Report |")
 	print("|--------------|")
 	# print("Hiiiiiiiiiiiiiii")
-	totalscore = 0
-	for q in ques:
-		print(q[0])
-		if q[2] == q[5]:
-			print("Correct Answer! - Marks awarded:",q[3])
-			totalscore += int(q[3])
-		else:
-			print("Wrong Answer! - Penality:", q[4])
-			totalscore += int(q[4])
-	print("Total Score",totalscore)
+	if flag == 1:
+		totalscore = 0
+		for q in ques:
+			print(q[0])
+			if q[2] == q[5]:
+				print("Correct Answer! - Marks awarded:",q[3])
+				totalscore += int(q[3])
+			else:
+				print("Wrong Answer! - Penality:", q[4])
+				totalscore += int(q[4])
+		print("Total Score",totalscore)
 
 def main():
+	global flag
+	flag = 0
 	global ques
 	ques = []
 	while True:
 		try:
 			line = input().split(" ")
 		except EOFError:
+			# print("Hiiiiiiiiiiiiiii")
 			break
 		# print(q_count)
 		if line[0] == "LOAD_QUESTIONS":
 			load_ques(line)
 		elif line[0] == "START_QUIZ":
 			start_quiz(line)
-		else:
+		elif line[0] == "SCORE_REPORT":
 			display(line)
 
 		
