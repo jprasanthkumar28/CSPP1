c273badd2afff58a91bbb85ecf251904cc35236f
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-13 08:39:11 +0530

    "cspp1_m10_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 5/10 "

--- R-Exam/Exam-3/CSPP1-Week3Exam/Quiz.py
+++ R-Exam/Exam-3/CSPP1-Week3Exam/Quiz.py
@@ -2,8 +2,23 @@ def check_response(data):
     # print(data)
     total_score = 0
     scored = {}
+    global flag
+    flag = 0
     full_Score = {}
     for text in data:
+        # print(type(int(text[4])))
+        try:
+            if int(text[4]) == type(int):
+                print("OKOK")
+        except ValueError:
+            print("Invalid")
+            flag = 1
+            break
+
+            
+            # print("Hi")
+        # if text[4] == "":
+        #     pass
         if text[0] not in scored:
                 full_Score[text[0]] = int(text[4])
                 # print(full_Score)
@@ -22,13 +37,14 @@ def check_response(data):
     # print(full_Score)
     for got in sorted(scored):
         for total in sorted(full_Score):
-            if got == total:
-                final = int((scored[got]/full_Score[total]) * 100)
-                if final <= 0:
-                    final = 0.0
-                    print(got + ": " + str(final) + "%")
-                else:
-                    print(got + ": " + str(float(final)) + "%")
+            if flag == 0:
+                if got == total:
+                    final = int((scored[got]/full_Score[total]) * 100)
+                    if final <= 0:
+                        final = 0.0
+                        print(got + ": " + str(final) + "%")
+                    else:
+                        print(got + ": " + str(float(final)) + "%")
 
 
     #     if int(value[0]) in dict1:
