d06ef9492811c89dc79a5696ed824c815436cff9
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-01-23 14:39:55 +0530

    "final_exam_a1_1: 4/14 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- Python/ToDo/ToDo.py
+++ Python/ToDo/ToDo.py
@@ -1,22 +1,27 @@
 def doTask(data):
 	# print(data)
 	strr = ""
+	flag = 0
 	if data[1] == '':
 		strr = "Title not provided"
+		flag = 1
 	else:
 		strr = data[1] + ", " + data[2] + ", "
-	if int(data[3]) < 0:
-	 	strr = "Invalid timeToComplete -1"
-	else:
-		strr += data[3]
-	if data[4] == 'y':
-		strr += ", Important"
-	else:
-		strr += ", Not Important"
-	if data[5] == 'y':
-		strr += ", Urgent"
-	else:
-		strr += ", Not Urgent"
+	if flag == 0:
+		if int(data[3]) < 0:
+		 	strr = "Invalid timeToComplete -1"
+		 	flag = 1
+		else:
+			strr += data[3]
+	if flag == 0:
+		if data[4] == 'y':
+			strr += ", Important"
+		else:
+			strr += ", Not Important"
+		if data[5] == 'y':
+			strr += ", Urgent"
+		else:
+			strr += ", Not Urgent"
 	if data[6] == "todo" or data[6] == "done":
 		strr += ", "+data[6]
 	else:
