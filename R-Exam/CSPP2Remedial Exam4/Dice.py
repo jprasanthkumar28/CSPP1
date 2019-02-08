0a30e7641b68dbc270f73272340bb8c913b29dd5
jprasanthkumar28 <jprasanthkumar28@msitprogram.net>
2019-02-08 10:36:39 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ R-Exam/CSPP2Remedial Exam4/Dice.py
@@ -0,0 +1,29 @@
+def main():
+    p_count = int(input())
+    result = {}
+    while True:
+        try:
+            # data = input()
+            data = input().split(" ")
+            if len(data) > 1:
+                # print(data)
+                if data[0] not in result and int(data[2] != 1):
+                    result[data[0]] = int(data[2])
+
+                elif data[2] == 1:
+                    result[data[0]] = 0
+
+                else:
+                    result[data[0]] += int(data[2])
+                # result[data] += int(data[2])
+        except EOFError:
+            break
+    # print(result)
+    for key, val in result.items():
+        val = list(result.values())
+        key = list(result.keys())
+        print(key[val.index(max(val))])
+
+
+if __name__ == '__main__':
+    main()
